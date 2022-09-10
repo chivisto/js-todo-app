b365568c0bd50fb421d6c3aeb19f302ba74f9296
Chivisto <isaaccovarr@gmail.com>
2022-09-09 23:11:41 -0600

    Add task commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const submit = (e) =>{
+const submit = (e) => {
     e.preventDefault();
     let input = document.querySelector('input');
     if (input.value != '')
@@ -6,11 +6,31 @@ const submit = (e) =>{
     input.value = '';
 }
 
+// add tasks
+const addTask = (task) =>{
+    let ul = document.querySelector('ul');
+    let li = document.createElement('li');
+    li.innerHTML = `<span class="delete">×</span><input type="checkbox"><label>${task}</label>`;
+    ul.appendChild(li);
+    document.querySelector('.tasksBoard').style.display = 'block';
+}
+
 loadEvents();
 // load every event in the page
 function loadEvents() {
     document.querySelector('form').addEventListener('submit', submit);
 }
+
+
+// function addTask(task) {
+//     let ul = document.querySelector('ul');
+//     let li = document.createElement('li');
+//     li.innerHTML = `<span class="delete">×</span><input type="checkbox"><label>${task}</label>`;
+//     ul.appendChild(li);
+//     document.querySelector('.tasksBoard').style.display = 'block';
+// }
+
+
 // subit data function
 // function submit1(e) {
 //     e.preventDefault();
