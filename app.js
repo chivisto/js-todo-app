011a2cb21a97cbae3688bfc47042b9129bebac3d
Chivisto <isaaccovarr@gmail.com>
2022-09-09 23:06:41 -0600

    added submit

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-loadEvents();
-// load every event in the page
-function loadEvents() {
-    document.querySelector('form').addEventListener('submit', submit);
-}
-// subit data function
-function submit(e) {
+const submit = (e) =>{
     e.preventDefault();
     let input = document.querySelector('input');
     if (input.value != '')
         addTask(input.value);
     input.value = '';
 }
+
+loadEvents();
+// load every event in the page
+function loadEvents() {
+    document.querySelector('form').addEventListener('submit', submit);
+}
+// subit data function
+// function submit1(e) {
+//     e.preventDefault();
+//     let input = document.querySelector('input');
+//     if (input.value != '')
+//         addTask(input.value);
+//     input.value = '';
+// }
 
